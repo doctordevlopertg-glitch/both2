0b583905e1a6e4377c7ac92e75eabee5efc69d4c
doctordevlopertg-glitch <doctordevlopertg@gmail.com>
2026-05-27 22:21:36 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "39683282")) #Replace with your api id
 API_HASH = environ.get("API_HASH", "ab1cc41ca283d480ebe386b1dce182f3") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "8618161742:AAHhCkXyHqSXemlQbs39wJ4pdBWNI4zjxdo") #Replace with your bot token
+BOT_TOKEN = environ.get("BOT_TOKEN", "8858777463:AAFPNifN-A6rlkn1d77_MMFj68iNW4YU2no") #Replace with your bot token
 
 # Database Configuration for MongoDB
 # Example: "mongodb://user:password@host:port/dbname"
